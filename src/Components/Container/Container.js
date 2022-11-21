5e6c3ef3bd02ed4c941c5879c3d091c76d2dddae
Yhucee14 <uchennachukwu2002@yahoo.com>
2022-11-21 01:14:05 -0800

    finished the Fixtures page

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Container.css';
 
 import Carousel from '../Carousel/Carousel'
+import Fixtures from '../Fixtures/Fixtures';
 
 const Container = () => {
   return (
@@ -9,6 +10,7 @@ const Container = () => {
         
         
         <Carousel />
+        <Fixtures />
 
     </div>
   )
---
+++ src/Components/Fixtures/Fixtures.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {Typography, IconButton} from "@mui/material";
+import "./Fixtures.css";
+import logo from '../Assets/logo.jpg';
+
+const Fixtures = () => {
+  return (
+    <div className='fixcon'>
+         <Typography variant='h4' sx={{ color: '#d72a2ada',}} >
+            Fixtures
+        </Typography><br></br>
+
+    <div className='fixcard'>
+
+        <div className='cardone'><span className='formred'>Last Game:</span> <br></br>
+        <IconButton size='small' edge='start' aria-label='logo'>
+       <img src={logo} alt="logo" width={25} />  
+       </IconButton> <b variant='h6' sx={{color: '#fff', paddingLeft: '2px'}}>Investors FC <b className='formgreen'>7</b></b>
+       
+        <b variant='h6' sx={{color: '#fff', paddingLeft: '2px'}}>-<b className='formgreen'>0 </b>Destiny Lodge</b>
+       <IconButton size='small' edge='end' aria-label='logo'>
+       <img src={logo} alt="logo" width={25} /> 
+      
+       </IconButton> <br></br>
+       <Typography className='formred'>Match details here</Typography>
+
+        </div>
+
+        <Typography>Form: <b className='formgreen'>W W</b> <b className='formred'>L L</b> D </Typography>
+
+        <div className='cardtwo'> <span className='formred'>Next Game:</span> <br></br>
+        <IconButton size='small' edge='start' aria-label='logo'>
+       <img src={logo} alt="logo" width={25} />  
+       </IconButton> <b variant='h6' sx={{color: '#fff', paddingLeft: '2px'}}>Investors FC <b className='formgreen'>0</b></b>
+       
+        <b variant='h6' sx={{color: '#fff', paddingLeft: '2px'}}>-<b className='formgreen'>0 </b>Unknown</b>
+       <IconButton size='small' edge='end' aria-label='logo'>
+       <img src={logo} alt="logo" width={25} /> 
+      
+       </IconButton>
+       <br></br>
+       <Typography>Next opponents Unknown</Typography>
+        </div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Fixtures
